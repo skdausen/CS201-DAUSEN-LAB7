d770cc11080acb4674dc8983dd8e3c5f8c911687
Sean Kelly Dausen <146722016+skdausen@users.noreply.github.com>
2023-10-11 00:37:46 -0700

    feat: Activity 7 in DSA
    if-else statement with .length and .includes in JavaScript

---
+++ script.js
@@ -0,0 +1,9 @@
+//Activity 7
+
+let password = "p@ssw0rd@2023";
+
+if (password.length >= 8 && password.includes('p@ssw0rd')) {
+    console.log('valid password');
+} else {
+    console.log('invalid password');
+}
